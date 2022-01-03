4f0845cfd25556dfcc4db2ed3cdd28422c607d8e
Diego Sandoval <diegosandovalr.corp@gmail.com>
2022-01-03 14:47:13 -0700

    User registration functionality completed

--- gpi/src/Components/RegisterForm/index.js
+++ gpi/src/Components/RegisterForm/index.js
@@ -3,6 +3,7 @@ import { Link, withRouter, useHistory } from "react-router-dom";
 import "react-notifications-component/dist/theme.css";
 import { store } from "react-notifications-component";
 import apis from "../../API";
+import "./styles.css";
 
 const RegisterForm = (props) => {
   let history = useHistory();
@@ -14,6 +15,7 @@ const RegisterForm = (props) => {
     academy: "",
     employeeNumber: "",
     password: "",
+    securityCode: "",
   });
 
   const handleClick = (e) => {
@@ -27,55 +29,71 @@ const RegisterForm = (props) => {
   const handleSubmitClick = async (e) => {
     e.preventDefault();
 
-    try {
-      const payload = {
-        userName: state.userName,
-        email: state.email,
-        firstName: state.firstName,
-        lastName: state.lastName,
-        academy: state.academy,
-        employeeNumber: state.employeeNumber,
-        password: state.password,
-      };
-      // Search for token
-      console.log(payload);
-      apis
-        .createUser(payload)
-        .then(() => {
-          history.push(`/login`);
-          store.addNotification({
-            title: "Cuenta creada",
-            message: "La cuenta de usuario ha sido creada con exito",
-            type: "default",
-            insert: "top",
-            container: "top-right",
-            animationIn: ["animate__animated", "animate__fadeIn"],
-            animationOut: ["animate__animated", "animate__fadeOut"],
-            dismiss: {
-              duration: 3500,
-              onScreen: true,
-            },
+    if (state.securityCode === "55468955") {
+      try {
+        const payload = {
+          userName: state.userName,
+          email: state.email,
+          firstName: state.firstName,
+          lastName: state.lastName,
+          academy: state.academy,
+          employeeNumber: state.employeeNumber,
+          password: state.password,
+        };
+        // Search for token
+        console.log(payload);
+        apis
+          .createUser(payload)
+          .then(() => {
+            history.push(`/login`);
+            store.addNotification({
+              title: "Cuenta creada",
+              message: "La cuenta de usuario ha sido creada con exito",
+              type: "default",
+              insert: "top",
+              container: "top-right",
+              animationIn: ["animate__animated", "animate__fadeIn"],
+              animationOut: ["animate__animated", "animate__fadeOut"],
+              dismiss: {
+                duration: 3500,
+                onScreen: true,
+              },
+            });
+          })
+          .catch((error) => {
+            // handle error
+            console.log(error);
+            store.addNotification({
+              title: "Usuario no creado",
+              message: "Error al crear el nuevo usuario",
+              type: "danger",
+              insert: "top",
+              container: "top-right",
+              animationIn: ["animate__animated", "animate__fadeIn"],
+              animationOut: ["animate__animated", "animate__fadeOut"],
+              dismiss: {
+                duration: 3500,
+                onScreen: true,
+              },
+            });
           });
-        })
-        .catch((error) => {
-          // handle error
-          console.log(error);
-          store.addNotification({
-            title: "Usuario no creado",
-            message: "Error al crear el nuevo usuario",
-            type: "danger",
-            insert: "top",
-            container: "top-right",
-            animationIn: ["animate__animated", "animate__fadeIn"],
-            animationOut: ["animate__animated", "animate__fadeOut"],
-            dismiss: {
-              duration: 3500,
-              onScreen: true,
-            },
-          });
-        });
-    } catch (error) {
-      alert("Error en el ingreso de los datos de usuario...");
+      } catch (error) {
+        alert("Error en el ingreso de los datos de usuario...");
+      }
+    } else {
+      store.addNotification({
+        title: "Codigo de seguridad incorrecto",
+        message: "Introdusca el codigo de seguridad correcto para continuar",
+        type: "danger",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3500,
+          onScreen: true,
+        },
+      });
     }
   };
 
@@ -85,7 +103,7 @@ const RegisterForm = (props) => {
         <div className="text-center">
           <h4 className="text-dark mb-4">Registro</h4>
         </div>
-        <form className={`needs-validation`} noValidate>
+        <form className={`needs-validation`} onSubmit={handleSubmitClick}>
           <div className="form-group">
             <input
               required={true}
@@ -128,6 +146,7 @@ const RegisterForm = (props) => {
             <div className="col-sm-6 mb-3 mb-sm-0">
               <select
                 id="academy"
+                required={true}
                 value={state.academy}
                 onChange={handleClick}
                 placeholder="Academia"
@@ -137,31 +156,29 @@ const RegisterForm = (props) => {
                   padding: "6px 12px",
                 }}
               >
-                <optgroup label="Academia/Departamento">
-                  <option value="Academia/Departamento">
-                    Academia/Departamento
-                  </option>
-                  <option value="Ingenieria en Sistemas Computacionales">
-                    Ingenieria en Sistemas Computacionales
-                  </option>
-                  <option value="Ingenieria Civil">Ingenieria Civil</option>
-                  <option value="Ingenieria Industrial">
-                    Ingenieria Industrial
-                  </option>
-                  <option value="Licenciatura en Administracion">
-                    Licenciatura en Administracion
-                  </option>
-                  <option value="Residencias Profesionales">
-                    Residencias Profesionales
-                  </option>
-                </optgroup>
+                <option value="" hidden>
+                  Academia/Departamento
+                </option>
+                <option value="Ingenieria en Sistemas Computacionales">
+                  Ingenieria en Sistemas Computacionales
+                </option>
+                <option value="Ingenieria Civil">Ingenieria Civil</option>
+                <option value="Ingenieria Industrial">
+                  Ingenieria Industrial
+                </option>
+                <option value="Licenciatura en Administracion">
+                  Licenciatura en Administracion
+                </option>
+                <option value="Residencias Profesionales">
+                  Residencias Profesionales
+                </option>
               </select>
             </div>
             <div className="col-sm-6">
               <input
                 required={true}
                 className="form-control form-control-user rounded-pill"
-                type="text"
+                type="number"
                 id="employeeNumber"
                 value={state.employeeNumber}
                 onChange={handleClick}
@@ -210,18 +227,19 @@ const RegisterForm = (props) => {
           <input
             required={true}
             className="form-control form-control-user rounded-pill"
-            type="text"
+            type="number"
             id="securityCode"
-            aria-describedby="emailHelp"
+            value={state.securityCode}
+            onChange={handleClick}
+            aria-describedby="securityCode"
             placeholder="Codigo de seguridad"
-            name="email"
+            name="securityCode"
           />
           <hr />
           <button
             className="btn btn-primary btn-block text-white btn-user rounded-pill py-2"
             id="registerBtn"
             type="submit"
-            onClick={handleSubmitClick}
           >
             Registrar cuenta
           </button>
